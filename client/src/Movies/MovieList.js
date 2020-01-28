decc1594d80959a729c012976700e32746243c11
Alec Dye <alecdye@gmail.com>
2020-01-28 12:21:35 -0800

    finishing part 1 checklist

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const MovieList = props => {
+  console.log("MovieList data", props)
   const [movies, setMovies] = useState([])
   useEffect(() => {
     const getMovies = () => {
@@ -14,10 +15,10 @@ const MovieList = props => {
           console.error('Server Error', error);
         });
     }
-    
+
     getMovies();
   }, []);
-  
+
   return (
     <div className="movie-list">
       {movies.map(movie => (
